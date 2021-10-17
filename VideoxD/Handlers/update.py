e97bfb343f35062baafc0cb74a11abff6e2fc000
Kritarth Shankar <74340401+Kody-K@users.noreply.github.com>
2021-10-17 20:52:09 +0530

    added update module (#5)

---
+++ VideoxD/Handlers/update.py
@@ -0,0 +1,14 @@
+import os
+from pyrogram import filters
+from .. import bot, chat_id
+from .. import admin_check
+
+@bot.on_message(filters.command("update") & filters.chat(chat_id))
+async def update(client, message):
+    admins = await admin_check(client, message)
+    if message.from_user.id not in admins:
+        return await message.reply("You Dont Have Sufficient Permissions!(Manage Video Chats)")
+    else:
+        return await message.reply("Updated and Restarted!")
+        os.system(f"cd ../../ && git pull && kill -9 {os.getpid()} && python3 -m VideoxD")
+
